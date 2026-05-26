f3f19c689581b59f8f4e380af8915f6e0b9b4c04
--unset <--unset>
2026-05-26 20:31:43 -0300

    feature: adiciona regras para criar avaliacao

--- backend/service/AvaliacaoService.js
+++ backend/service/AvaliacaoService.js
@@ -1,16 +1,30 @@
 const avaliacaoRepository = require('../repository/AvaliacaoRepository');
 const serviceRepository = require('../repository/ServiceRepository');
 const empresaRepository = require('../repository/EmpresaRepository');
+const logger = require('../config/logger');
+
 
 class AvaliacaoService {
     // CREATE  =  POST
     // Valida campos obrigatórios e existência de serviço e empresa antes de criar a avalição
-    async createAvaliacao(avaliacaoData) {
+    async createAvaliacao(avaliacaoData, serviceId, empresaId) {
 
         // Garantir existencia do serviço
-        // Garantir status do serviço finalizado
-        // Garantir existencia da empresa
+        const service = await serviceRepository.findById(serviceId);
+        if (!service) {
+            throw new Error('Serviço não encontrada.');
+        }
 
+        // Garantir status do serviço finalizado
+        if (service.status_solicitacao !== 'CONCLUIDO'){
+            throw new Error('Serviço cancelado ou ainda não finalizado.');
+        }
+
+        // Garantir existencia da empresa
+        const empresa = await serviceRepository.findById(empresaId);
+        if (!empresa) {
+            throw new Error('Serviço não encontrada.');
+        }
     }
 
     async getAllAvaliacao(filters = {}) {

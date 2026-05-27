63e0f839d448c09719da14aa7c19e13e1e108737
Elieuton da silva lemos junior <160804009+lemosjr@users.noreply.github.com>
2026-05-27 12:17:06 -0300

    :sparkles: feat: entrega da revisão de especialidades, testado e aprovado

--- backend/config/swagger/schemas/index.js
+++ backend/config/swagger/schemas/index.js
@@ -10,7 +10,7 @@ const schemas = {
     ...serviceSchemas,
     ...empresaSchemas,
     ...historicoSchemas,
-    ...especialidadeSchemas
+    ...especialidadeSchemas,
     ...paginationSchema
 };
 
